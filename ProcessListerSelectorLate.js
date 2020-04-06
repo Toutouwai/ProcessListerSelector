1dca13f1727ec8bf420f9dcce26ade1a773b9a80
Robin Sallis <Toutouwai@users.noreply.github.com>
2020-04-06 17:49:53 +1200

    v0.1.0

---
+++ ProcessListerSelectorLate.js
@@ -0,0 +1,29 @@
+(function($) {
+
+	function setFilters() {
+		$('#ProcessListerFilters').val($('#ls_selector').val()).change();
+	}
+
+	$(document).ready(function() {
+
+		// Move "Selector" tab and focus it
+		var $selector_tab = $('#_ListerSelectorTab').parent('li');
+		$selector_tab.parent().prepend($selector_tab);
+		$selector_tab.find('a').click();
+
+		// Remove change event handler from filter input so it's possible to manually set it
+		$(document).off('change', '.InputfieldSelector :input:not(.select-field):not(.input-value-autocomplete)').on('change', '.InputfieldSelector :input:not(.select-field):not(.input-value-autocomplete):not([id=ProcessListerFilters])', function() {
+			InputfieldSelector.changeAny($(this));
+		});
+
+		// Table header rows are not used for sorting
+		ProcessLister.results.off('click', '.ProcessListerTable > thead th');
+
+		// Sync selector to filters
+		$('#ls_selector').change(setFilters);
+
+		setFilters();
+
+	});
+
+}(jQuery));
---
+++ README.md
@@ -0,0 +1,5 @@
+# Lister Selector
+
+A module for ProcessWire CMS/CMF. Use a selector string to list items with Lister/ListerPro.
+
+Full readme to come.
